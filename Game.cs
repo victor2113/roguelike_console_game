c5d92dfc96918006c162ecfa0bb26d460159ea66
Илья Игнатов <80439643+victor2113@users.noreply.github.com>
2023-04-04 16:48:30 +1000

    Add files via upload
    движение игрока, карта (врагов пока убрал) , меню норм

--- Game.cs
+++ Game.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace RogueFefu
 {
     internal class Game
     {
+        private const int ARROWUP = 0;
+        private const int ARROWDOWN = 1;
+        private const int ARROWLEFT = 2;
+        private const int ARROWRIGHT = 3;
+        private const int ESCAPE = 4;
+
+
+
+        private bool GameOver = false;
+        private int ButtonPressed;
+
+        public MapLevel CurrentMap { get; set; }
+        public int CurrentLevel { get; set; }
+        public Player CurrentPlayer { get; }
+        public int CurrentTurn { get; set; }
+
+
+
+        public Game()
+        {
+            string PlayerName = "Tolya";
+            this.CurrentLevel = 1;
+            this.CurrentMap = new MapLevel();
+            this.CurrentPlayer = new Player(PlayerName);
+            this.CurrentPlayer.Location = CurrentMap.PlaceMapCharacter(Player.CHARACTER, true);
+        }
+
+
+        public Game(string PlayerName)
+        {
+            this.CurrentLevel = 1;
+            this.CurrentMap = new MapLevel();
+            this.CurrentPlayer = new Player(PlayerName);
+            this.CurrentPlayer.Location = CurrentMap.PlaceMapCharacter(Player.CHARACTER, true);
+        }
 
         public void Begin()
         {
             Console.CursorVisible = false;
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-            Console.WriteLine("╔════════════════════════════════════════════════════╗");
-            Console.WriteLine("║                                                    ║");
-            Console.WriteLine("║          ██╗  ██╗███████╗██████╗  ██████╗          ║");
-            Console.WriteLine("║          ██║  ██║██╔════╝██╔══██╗██╔═══██╗         ║");
-            Console.WriteLine("║          ███████║█████╗  ██████╔╝██║   ██║         ║");
-            Console.WriteLine("║          ██╔══██║██╔══╝  ██╔══██╗██║   ██║         ║");
-            Console.WriteLine("║          ██║  ██║███████╗██║  ██║╚██████╔╝         ║");
-            Console.WriteLine("║          ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝          ║");
-            Console.WriteLine("║                                                    ║");
-            Console.WriteLine("║            Please, enter your player name          ║");
-            Console.WriteLine("║                                                    ║");
-            Console.WriteLine("╚════════════════════════════════════════════════════╝");
-            Console.Write("Name: ");
-
-            string playerName = Console.ReadLine();
-            Player player = new Player(playerName);
-
             string promt = $@"
                                                                                                                  
 ███████╗███████╗███████╗██╗   ██╗    ██████╗ ██╗   ██╗███╗   ██╗ ██████╗ ███████╗ ██████╗ ███╗   ██╗             
@@ -43,8 +59,7 @@ namespace RogueFefu
 ██║     ███████╗██║     ╚██████╔╝    ██████╔╝╚██████╔╝██║ ╚████║╚██████╔╝███████╗╚██████╔╝██║ ╚████║             
 ╚═╝     ╚══════╝╚═╝      ╚═════╝     ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝   
                                                                               RogueLike game project
-
-Hello, {player.PlayerName}! Let's start the game!
+Hello, {this.CurrentPlayer.PlayerName}! Let's start the game!
 Use the arrow keys to choose options and press enter to select one";
 
 
@@ -59,7 +74,7 @@ Use the arrow keys to choose options and press enter to select one";
             switch (selectedIndex)
             {
                 case 0:
-                    RunTheGame();
+                    LoadmapAndPlay(); 
                     break;
                 case 1:
                     AboutGameText();
@@ -75,6 +90,9 @@ Use the arrow keys to choose options and press enter to select one";
 
         }
 
+
+
+
         private void ExitGame()
         {
             Console.Clear();
@@ -89,52 +107,119 @@ Use the arrow keys to choose options and press enter to select one";
             Console.ReadKey(true);
             Begin();
         }
-        private void RunTheGame()
-        {
-            string[] options = { "Next", "Generate", "Exit" };
-            StartMenu startMenu = new StartMenu(options, "Map");
-            int selectedIndex = startMenu.Run();
-            switch (selectedIndex)
-            {
-                case 0:
-                    Console.Clear();
-                    LoadMapLevel();
-                    Console.ReadKey(true);
-                    RunTheGame();
-
-
-                    break;
-                case 1:
-                    Generate();
-                    Console.Clear();
-                    LoadMapLevel();
-                    Console.ReadKey(true);
-                    RunTheGame();
-                    break;
-                case 2:
-                    ExitGame();
-                    break;
-
-
-            }
-
-            Console.ReadKey(true);
-        }
+ 
+        
+        
+        
+        
+        
+        
+        
         private void LoadMapLevel()
         {
-            MapLevel newLevel = new MapLevel();
-            string level = newLevel.MapText();
+            string level = this.CurrentMap.MapText();
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine(level);
-
         }
-        private void Generate()
+
+        
+
+
+
+
+        void LoadmapAndPlay()
         {
-            for (int i = 0; i < 101; i++)
-                LoadMapLevel();
+            Console.Clear();
+            LoadMapLevel();
+            do
+            {
+                int key = ReadKeyPressedByPlayer();
+                switch (key)
+                {
+                    case ARROWUP:
 
+                        MoveCharacter(CurrentPlayer, MapLevel.Direction.North);
+                        Console.Clear();
+                        LoadMapLevel();
+                        break;
+                    case ARROWDOWN:
+                        MoveCharacter(CurrentPlayer, MapLevel.Direction.South);
+                        Console.Clear();
+                        LoadMapLevel();
+                        break;
+                    case ARROWLEFT:
+                        MoveCharacter(CurrentPlayer, MapLevel.Direction.West);
+                        Console.Clear();
+                        LoadMapLevel();
+                        break;
+                    case ARROWRIGHT:
+                        MoveCharacter(CurrentPlayer, MapLevel.Direction.East);
+                        Console.Clear();
+                        LoadMapLevel();
+                        break;
+                    case ESCAPE:
+                        ExitGame();
+                        break;
+                }
+            } while (!GameOver);
+    }
+
+         
+
+
+
+        private int ReadKeyPressedByPlayer()
+        {
+            ConsoleKey keyPressed;
+           
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    ButtonPressed = 0;
+                  
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    ButtonPressed = 1 ;
+                }
+                else if (keyPressed == ConsoleKey.LeftArrow)
+                {
+                    ButtonPressed = 2;
+                }
+                else if (keyPressed == ConsoleKey.RightArrow)
+                {
+                    ButtonPressed = 3;
+                }
+            else if (keyPressed == ConsoleKey.Escape)
+            {
+                ButtonPressed = 4;
+            }
+            return ButtonPressed;
         }
 
+        public void MoveCharacter(Player player, MapLevel.Direction direct)
+        {
+            // Move character if possible.
+
+            // List of characters a living character can move onto.
+            List<char> charsAllowed =
+                new List<char>(){MapLevel.ROOM_INT, MapLevel.STAIRWAY,
+                MapLevel.ROOM_DOOR, MapLevel.HALLWAY};
+
+            // Set surrounding characters
+            Dictionary<MapLevel.Direction, MapSpace> surrounding =
+                CurrentMap.SearchAdjacent(player.Location.X, player.Location.Y);
+
+            // If the map character in the chosen direction is habitable 
+            // and if there's no monster there,move the character there.
+            
+            if (charsAllowed.Contains(surrounding[direct].MapCharacter) &&
+                surrounding[direct].DisplayCharacter == null)
+                    player.Location = CurrentMap.MoveDisplayItem(player.Location, surrounding[direct]);
+
+        }
     }
 
 }
